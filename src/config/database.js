d0316f589116dd3651eb26f11145e9e89dfc2584
Leandro <leandrodmax@gmail.com>
2019-10-20 20:43:14 -0300

    :art: Improve structure and add sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  username: "root",
+  password: null,
+  database: "psp",
+  host: "127.0.0.1",
+  dialect: "sqlite",
+  storage: "./database/database.sqlite",
+  operatorsAliases: false,
+  define: {
+    timestamps: true,
+  }
+}
