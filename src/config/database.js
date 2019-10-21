4edf05da76a1faab7f21d1e2f0cc58b232d4ec57
Leandro <leandrodmax@gmail.com>
2019-10-21 19:10:28 -0300

    :fix: Fix database path

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,7 @@ module.exports = {
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
-  storage: "./database/database.sqlite",
-  operatorsAliases: false,
+  storage: "./src/database/database.sqlite",
   define: {
     timestamps: true,
     underscored: true,
