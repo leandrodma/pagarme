f196d953b12a32f5fef7bfbc4dbf4f2712d06be0
Leandro <leandrodmax@gmail.com>
2019-10-22 18:58:48 -0300

    :white_check_mark: Include routes test

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,4 +4,8 @@ routes.get('/healthcheck', function (req, res, next) {
 	res.json({status: 'UP'});
 });
 
+routes.post('/clients/:id/checkout', (req, res, next) => {
+	res.sendStatus(200)
+});
+
 module.exports = routes;
--- tests/integration/e2e/healthcheck.test.js
+++ tests/integration/e2e/healthcheck.test.js
@@ -3,9 +3,21 @@ const app = require('../../../src/app/app');
 
 // fecha as conexões com o servidor, quando terminar os testes
 
-describe('inicio dos testes inerentes a aplicação', () => {
+describe('Testes de endpoints da aplicação', () => {
   test('Testa rota de healthCheck', async () => {
       const response = await request(app).get('/healthcheck')
       expect(response.body).toEqual({ status: 'UP' })
     });
+
+    test('Testa rota de checkout', async () => {
+      await request(app)
+            .post('/clients/1/checkout')
+            .expect(200)
+    });
 });
+
+describe('Testes para o checkout', () => {
+  test('Teste de payload no checkout com cliente inválido', () => {
+    
+  })
+})
